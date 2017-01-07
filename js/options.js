44deb2b0301d3a813b939492d38ba304261beba4
Oliver Käfer <ernst.kaefer@gmail.com>
2017-01-07 21:12:48 +0100

    added deleting Cookies (all and since)

--- js/options.js
+++ js/options.js
@@ -12,6 +12,7 @@ function dateToRequiredFormat(date) {
 }
 
 function getDomainFromURLString(url) {
+    var domain;
     if (url.indexOf("://") > -1){
         domain = url.split("/")[2];
     } else {
@@ -21,7 +22,32 @@ function getDomainFromURLString(url) {
     return domain;
 }
 
+var thirdPartyCookieAllowed;
+var cookieLiveTime;
+
+chrome.contentSettings.cookies.get({
+    "primaryUrl": "http://google.*"
+},function (details) {
+    var val = details.setting;
+    if (val === "allow"){
+        cookieLiveTime = 0;
+    } else if (val === "session_only"){
+        cookieLiveTime = 1;
+    } else {
+        cookieLiveTime = 2;
+    }
+});
+chrome.privacy.websites.thirdPartyCookiesAllowed.get({},function (details) {
+    thirdPartyCookieAllowed = details.value;
+});
+
 $(document).ready(function () {
+    if (thirdPartyCookieAllowed){
+        $("#thirdPartyGeneral").attr("src","img/checked.png");
+    }
+    if (cookieLiveTime !== undefined){
+        document.getElementById('cookieLivetimeGeneral').selectedIndex = cookieLiveTime;
+    }
     var historybtn = document.getElementById("historybtn");
     var startofyear = new Date();
     startofyear.setMonth(0,1);
@@ -63,5 +89,12 @@ $(document).ready(function () {
                 $("#settings").show();
             });
         });
+    });
+    $("#removeAllCookies").click(function () {
+        chrome.browsingData.removeCookies({"since":0});
+    });
+    $("#removeSince").click(function () {
+        var since = new Date($("#since").val());
+        chrome.browsingData.removeCookies({"since":since.getTime()});
     });
 });
