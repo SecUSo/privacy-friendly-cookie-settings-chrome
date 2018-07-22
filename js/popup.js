0beccdeb2965c1d6c52a2f783a747cf2b0362ab5
lauraimler <laurab94@live.de>
2018-07-22 23:59:11 +0200

    Add language: english

--- js/popup.js
+++ js/popup.js
@@ -28,13 +28,13 @@ function setCookieLivetimeSetting(pattern,value) {
 }
 
 $(document).ready(function () {
-	
+	var url;
 	addText();
     chrome.tabs.query({
         active: true,
         currentWindow: true
     },function (tabs) {
-        var url = getDomainFromURLString(tabs[0]['url']);
+        url = getDomainFromURLString(tabs[0]['url']);
         $("#side_url").text(chrome.i18n.getMessage("websiteSettings")+ " " +url);
         getCookieLivetimeSetting(function (response) {
             var cookielivetime = response.cookieLivetime;
@@ -44,23 +44,23 @@ $(document).ready(function () {
         },url);
     });
     $("#cookieLivetime").on("change",function (e) {
-        var url = $("#side_url").text();
-        url = url.indexOf("http") === -1 ? "*://" + url + "/*" : url + "/*";
-        setCookieLivetimeSetting(url,this.selectedIndex);
+        var urltemp = url; 
+        urltemp = urltemp.indexOf("http") === -1 ? "*://" + urltemp + "/*" : urltemp + "/*";
+        setCookieLivetimeSetting(urltemp,this.selectedIndex);
     });
     chrome.storage.sync.get(null,function (items) {
-        var url = $("#side_url").text();
-        if (items[url]){
+        var urltemp = url; 
+        if (items[urltemp]){
             $("#logincookies").attr("src","img/checked32.png");
         }
         else {
             $("#logincookies").attr("src","img/not_checked32.png");
         }
         if (items["thirdparty"]){
-            if (items["thirdparty"][url]) {
+            if (items["thirdparty"][urltemp]) {
                 $("#thirdparty").attr("src", "img/checked32.png");
             }
-            else if (items["thirdparty"][url] === false){ // items["thirdparty"][url] could be undefined
+            else if (items["thirdparty"][urltemp] === false){ // items["thirdparty"][url] could be undefined
                 $("#thirdparty").attr("src", "img/not_checked32.png");
             }
             else {
@@ -77,35 +77,35 @@ $(document).ready(function () {
     });
     $("#logincookies").click(function () {
         var src = $("#logincookies").attr("src");
-        var url = $("#side_url").text();
+        var urltemp = url;
         var obj = {};
         chrome.storage.sync.get("logincookies",function (items) {
             if (src.indexOf("not") === -1){
-                items.logincookies[url]["setting"] = false;
+                items.logincookies[urltemp]["setting"] = false;
                 chrome.storage.sync.set(obj);
                 $("#logincookies").attr("src","img/not_checked32.png");
             } else {
-                items.logincookies[url]["setting"] = true;
+                items.logincookies[urltemp]["setting"] = true;
                 chrome.storage.sync.set(obj);
                 $("#logincookies").attr("src","img/checked32.png");
             }
         });
     });
     $("#thirdparty").click(function () {
-        var url = $("#side_url").text();
+        var urltemp = url;
         chrome.storage.sync.get("thirdparty",function (items) {
             var settings = false;
             if (!items.hasOwnProperty("thirdparty")){
                 items = {thirdparty : {}};
             } else {
-                settings = items["thirdparty"][url] || false;
+                settings = items["thirdparty"][urltemp] || false;
             }
             if (settings){
-                items["thirdparty"][url] = false;
+                items["thirdparty"][urltemp] = false;
                 $("#thirdparty").attr("src","img/not_checked32.png");
             }
             else {
-                items["thirdparty"][url] = true;
+                items["thirdparty"][urltemp] = true;
                 $("#thirdparty").attr("src","img/checked32.png");
             }
             chrome.storage.sync.set(items);
